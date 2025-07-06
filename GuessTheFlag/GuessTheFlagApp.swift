a484926cb3bf7ca1a4c338764c1a2272ddbb739d
Deser7 <deser7@icloud.com>
2025-07-06 13:41:33 +0400

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Наташа Спиридонова on 06.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
